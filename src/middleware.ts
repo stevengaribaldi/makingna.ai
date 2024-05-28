08a20b27bf22f005b31220cbf225300e2fa27fd5
AcevedoEsteban <astevenaa@gmail.com>
2024-05-28 16:29:41 -0700

    [frontend]style:tailwind styles needed added

---
+++ src/middleware.ts
@@ -0,0 +1,9 @@
+import { auth, authMiddleware } from "@clerk/nextjs";
+export default authMiddleware({
+ publicRoutes: ['/', '/auth(.*)', '/portal(.*)'],
+ignoredRoutes:['/chatbot']
+});
+
+export const config = {
+  matcher: ['/((?!.+.[w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+}
