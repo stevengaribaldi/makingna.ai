5bd8140b9305677cdfd06d5ccd6fec884a4db6a9
AcevedoEsteban <astevenaa@gmail.com>
2024-05-29 22:49:29 -0700

    [fe]feat(auth):add context for managing authentication steps

--- src/context/use-auth-context.tsx
+++ src/context/use-auth-context.tsx
@@ -9,3 +9,28 @@ const InitialValues: InitialValuesProps = {
   currentStep: 1,
   setCurrentStep: () => undefined,
 };
+
+const authContext = React.createContext(InitialValues);
+const { Provider } = authContext;
+
+export const AuthContextPorvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [currentStep, setCurrentStep] = useState<number>(
+    InitialValues.currentStep,
+    );
+    const values = {
+        currentStep,
+        setCurrentStep,
+
+    }
+    return <Provider value={values}>{children}</Provider>
+};
+
+
+export const useAuthContextHook = () => {
+    const state = React.useContext(authContext);
+    return state;
+}
